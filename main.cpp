8ba3498e58cab43b7f1ca92126698cbb8a4b18b7
Amina <amina.mamadj@gmail.com>
2025-04-14 07:04:27 +0600

    Добавлена генерация лабиринта DFS

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    cout << "Maze Generator Project (C++)" << endl;
+    return 0;
+}
