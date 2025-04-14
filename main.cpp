af1e70442361272f126bc4de62f8d8ca4dcf3a11
Amina <amina.mamadj@gmail.com>
2025-04-14 07:07:29 +0600

    Добавленны переменные и структура лабиринта

--- main.cpp
+++ main.cpp
@@ -9,3 +9,11 @@ int main() {
     cout << "Maze Generator Project (C++)" << endl;
     return 0;
 }
+
+const char WALL = '#';
+const char PATH = ' ';
+const int DEFAULT_ROWS = 21;
+const int DEFAULT_COLS = 21;
+
+int rows = DEFAULT_ROWS, cols = DEFAULT_COLS;
+vector<vector<char>> maze;
