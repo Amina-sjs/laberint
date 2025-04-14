c54f7db4ce0981b879b92bc50ba06651c51cfa67
Amina <amina.mamadj@gmail.com>
2025-04-14 07:11:47 +0600

    Добавлена функция отображения лабиринта

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     cout << "Maze Generator Project (C++)" << endl;
     srand(time(0));
     initMaze();
+    printMaze();
     return 0;
 }
 
@@ -22,4 +23,13 @@ vector<vector<char>> maze;
 
 void initMaze() {
     maze = vector<vector<char>>(rows, vector<char>(cols, WALL));
+}
+
+void printMaze() {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << maze[i][j];
+        }
+        cout << '\n';
+    }
 }
